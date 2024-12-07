9d902de200b79530a7397ba107071fd1b2ca3209
lebit-L1x <lebitl1x@gmail.com>
2024-12-07 14:44:52 +0700

    blynk config

--- Client/Client.ino
+++ Client/Client.ino
@@ -1,6 +1,46 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
+#include <WiFiClient.h>
 
 //Blynk Config
 #define BLYNK_TEMPLATE_ID "TMPL6hrV-kNLf"
 #define BLYNK_TEMPLATE_NAME "ProyekAkhirIOT"
+#define BLYNK_AUTH_TOKEN "mcZESjQPRAC1Pi2mAwtbH-GLQhVRYVvf"
+#define BLYNK_PRINT Serial
+
+#include <BlynkSimpleEsp32.h>
+
+//Wifi
+char ssid[] = "fanculo"; //Nama WiFi yang digunakan
+char pass[] = "00000000"; //Password WiFi yang digunakan
+
+//Components
+#define LED 2
+
+int led_state = 0; //Default
+
+BLYNK_WRITE(V0) {
+  int led_state = param.asInt();
+  if (led_state == 1) {
+    digitalWrite(LED, HIGH);
+    Serial.println("ON");
+  } else {
+    digitalWrite(LED, LOW);
+    Serial.println("OFF");
+  }
+}
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  //Pin Declaration
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  Blynk.run();
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-"# FinproIOT" 
-
+# FinproIOT
 References: https://esp32io.com/tutorials/esp32-solenoid-lock
+
 https://randomnerdtutorials.com/esp32-cam-video-streaming-face-recognition-arduino-ide/ 
